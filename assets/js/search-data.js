46bd05d51357ca910e4c6e39578548fc008e1562
mariekemeelen <34003042+mariekemeelen@users.noreply.github.com>
2026-01-20 15:44:50 +0000

    Deploying to gh-pages from @ mariekemeelen/mariekemeelen.github.io@59387d6059d004061fd3c10030bfd07f56884955 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,14 +518,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%6D%6D%39%38%36@%63%61%6D.%61%63.%75%6B", "_blank");
         },
       },{
         id: 'social-rss',
@@ -539,14 +532,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=ziHwb6EAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-bluesky',
+        title: 'Bluesky',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://bsky.app/profile/mariekem.bsky.social", "_blank");
         },
       },{
       id: 'light-theme',
